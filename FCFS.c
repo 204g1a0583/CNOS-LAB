9c863b4783d4831144b766e2dfc08e57f53f5d27
204g1a0583 <94512805+204g1a0583@users.noreply.github.com>
2022-09-16 15:18:24 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 main()
 {
 int bt[20], wt[20], tat[20], i, n;
@@ -27,5 +26,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
 }
